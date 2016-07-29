32bf9747924dda5fad9f29e6bcd90755f37560ad
vnataliia <nvelyka@gmail.com>
2016-07-29 12:26:16 +0300

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -59,7 +59,7 @@ for (var i = 0; i < names.length; i++) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (firstLetter == 'J') {
+  if (firstLetter == 'j') {
     byeSpeaker.speak(names[i]);
   } else {
     helloSpeaker.speak(names[i]);
