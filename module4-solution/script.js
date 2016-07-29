b573badadaa2761854a2338cb5509977a04a4988
vnataliia <nvelyka@gmail.com>
2016-07-29 12:19:36 +0300

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -52,7 +52,7 @@ for (var i = 0; i < names.length; i++) {
   // to lower case character 'j' afterwards.
   // Look up these methods on Mozilla Developer Network web site if needed.
 
-  var firstLetter = names[i].charAt(0);
+  var firstLetter = names[i].charAt(0).toLowerCase();
 
   // STEP 12:
   // Compare the 'firstLetter' retrieved in STEP 11 to lower case
